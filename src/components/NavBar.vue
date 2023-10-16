9dda97e76adb791282a72612360b16681f23084a
David <osodavid001@gmail.com>
2023-10-16 08:30:32 +0100

    navbar hover

--- src/components/NavBar.vue
+++ src/components/NavBar.vue
@@ -4,12 +4,12 @@
             <div class="text-white h-7 m-10 px-7 text-4xl font-bold">
                 get<span class="text-dark-purple">linked</span>
             </div>
-            <div class="text-white w-2/4 flex justify-around h-9 items-center">
-                <a href="#">Timeline</a>
-                <a href="#">Overview</a>
-                <a href="#">FAQs</a>
-                <a href="#">Contact</a>
-                <a href="#" class="bg-primary-colour rounded-sm py-1 px-6 ml-10">Register</a>
+            <div class="text-white w-2/4 flex justify-around h-9 items-center hover:group-hover:text-black">
+                <a href="#" class="hover:text-primary-colour">Timeline</a>
+                <a href="#" class="hover:text-primary-colour">Overview</a>
+                <a href="#" class="hover:text-primary-colour">FAQs</a>
+                <a href="#" class="hover:text-primary-colour">Contact</a>
+                <a href="#" class="bg-primary-colour rounded-sm py-1 px-6 ml-10 hover:bg-light-purple">Register</a>
             </div>
         </nav>
     </header>
